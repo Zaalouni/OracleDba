53e4e14935777a01b3f976d5a7c54ee2a79ad9d1
Zaalouni <43424197+Zaalouni@users.noreply.github.com>
2019-09-10 09:25:17 +0200

    Update DB_How_to_check_hidden_parameter_Oracle.sql

--- DB_How_to_check_hidden_parameter_Oracle.sql
+++ DB_How_to_check_hidden_parameter_Oracle.sql
@@ -11,9 +11,17 @@ SET lines 200
 col name for a50
 col value for a40
 
-select ksppinm name , ksppstvl value from x$ksppi a, x$ksppsv b where a.indx=b.indx and substr(ksppinm,1,1) = '_';
+SELECT  ksppinm name , ksppstvl value from x$ksppi a, x$ksppsv b where a.indx=b.indx and substr(ksppinm,1,1) = '_';
 
 
+SELECT a.ksppinm AS parameter,
+       a.ksppdesc AS description,
+       b.ksppstvl AS session_value,
+       c.ksppstvl AS instance_value
+FROM   x$ksppi a,        x$ksppcv b,        x$ksppsv c 
+WHERE  a.indx = b.indx
+AND    a.indx = c.indx   AND    a.ksppinm LIKE '/_%' ESCAPE '/'
+ORDER BY a.ksppinm;
 
 --------------------------------------------------------------------------------------
 -- File Name    : https://github.com/Zaalouni/OracleDba
