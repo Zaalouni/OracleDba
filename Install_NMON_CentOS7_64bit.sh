d2b1c9ab68c9fac70af11434a18901470bef8e05
Zaalouni <43424197+Zaalouni@users.noreply.github.com>
2019-09-10 09:18:29 +0200

    Update Install_NMON_CentOS7_64bit.sh

--- Install_NMON_CentOS7_64bit.sh
+++ Install_NMON_CentOS7_64bit.sh
@@ -7,12 +7,10 @@
 -- -----------------------------------------------------------------------------------
 
 
-
-# Get Root
+# Get user Root
 sudo su
 
- 
-# Download NMON archive
+ # Download NMON v16E from archive
 
 cd /tmp
 
@@ -21,18 +19,15 @@ wget http://sourceforge.net/projects/nmon/files/nmon16e_mpginc.tar.gz
 # Untar archive
 tar -xzvf nmon16e_mpginc.tar.gz
  
-# Copy nmon file
+# Copy nmon file to bin folder
 cp nmon_x86_64_centos7 /usr/local/bin/
 chmod a+x /usr/local/bin/nmon_x86_64_centos7 
  
 # Create symbolic link
 ln -s /usr/local/bin/nmon_x86_64_centos7 /usr/local/bin/nmon
 
-# tidy up tmp
+# delete clean old nome files from  tmp folder
 rm -f nmon_*
-
-
-
 
 
 
